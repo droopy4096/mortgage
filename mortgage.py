4d8e9ae352f8d428f7fdd449036c53763cd042b5
Dmitry Makovey <dmakovey@stanford.edu>
2017-04-04 12:44:31 -0700

    expand calculations to include all mortgage related costs as well as add ROI calculator

--- mortgage.py
+++ mortgage.py
@@ -8,6 +8,12 @@ import decimal
 MONTHS_IN_YEAR = 12
 DOLLAR_QUANTIZE = decimal.Decimal('.01')
 
+class MissingValue(Exception):
+    def __init__(self,val):
+        self._val=val
+    def __str__(self):
+        return str(self._val)
+
 def dollar(f, round=decimal.ROUND_CEILING):
     """
     This function rounds the passed float to 2 decimal places.
@@ -17,66 +23,98 @@ def dollar(f, round=decimal.ROUND_CEILING):
     return f.quantize(DOLLAR_QUANTIZE, rounding=round)
 
 class Mortgage:
-    def __init__(self, interest, months, amount):
+    # def __init__(self, interest, months, amount):
+    def __init__(self, interest, months, 
+                  house_price=None, downpayment=None,
+                  amount=None,
+                 ):
         self._interest = float(interest)
         self._months = int(months)
-        self._amount = dollar(amount)
+        if house_price and downpayment:
+            self._amount = dollar(house_price)-dollar(downpayment)
+            self._house_price=dollar(house_price)
+        elif amount and house_price:
+            self._amount = dollar(amount)
+            self._house_price=dollar(house_price)
+        else:
+            raise MissingValue("need to specify house price and either load amount or downpayment")
+        # self._property_tax=property_tax
+        # self._investments=investments
+        # self._insurance_annual=annual_insurance
 
+    @property
+    def house_price(self):
+        return self._house_price
+
+    @property
     def rate(self):
         return self._interest
 
+    @property
     def month_growth(self):
         return 1. + self._interest / MONTHS_IN_YEAR
 
+    @property
     def apy(self):
-        return self.month_growth() ** MONTHS_IN_YEAR - 1
+        return self.month_growth ** MONTHS_IN_YEAR - 1
 
+    @property
     def loan_years(self):
         return float(self._months) / MONTHS_IN_YEAR
 
+    @property
     def loan_months(self):
         return self._months
 
+    @property
     def amount(self):
         return self._amount
 
+    @property
     def monthly_payment(self):
-        pre_amt = float(self.amount()) * self.rate() / (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth()) ** self.loan_months()))
+        pre_amt = float(self.amount) * self.rate / (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth) ** self.loan_months))
         return dollar(pre_amt, round=decimal.ROUND_CEILING)
 
     def total_value(self, m_payment):
-        return m_payment / self.rate() * (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth()) ** self.loan_months()))
+        return m_payment / self.rate * (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth) ** self.loan_months))
 
+    @property
     def annual_payment(self):
-        return self.monthly_payment() * MONTHS_IN_YEAR
+        return self.monthly_payment * MONTHS_IN_YEAR
 
+    @property
     def total_payout(self):
-        return self.monthly_payment() * self.loan_months()
+        return self.monthly_payment * self.loan_months
+
+    def monthly_prepayment(self,month):
+        return 0
 
     def monthly_payment_schedule(self):
-        monthly = self.monthly_payment()
-        balance = dollar(self.amount())
-        rate = decimal.Decimal(str(self.rate())).quantize(decimal.Decimal('.000001'))
-        while True:
+        monthly = self.monthly_payment
+        balance = dollar(self.amount)
+        rate = decimal.Decimal(str(self.rate)).quantize(decimal.Decimal('.000001'))
+        # while True:
+        for month in range(1,self._months):
             interest_unrounded = balance * rate * decimal.Decimal(1)/MONTHS_IN_YEAR
             interest = dollar(interest_unrounded, round=decimal.ROUND_HALF_UP)
-            if monthly >= balance + interest:
+            pmt=monthly+self.monthly_prepayment(month)
+            if pmt >= balance + interest:
                 yield balance, interest
                 break
-            principle = monthly - interest
-            yield principle, interest
-            balance -= principle
+            principal = pmt - interest
+            yield principal, interest
+            balance -= principal
 
 def print_summary(m):
-    print('{0:>25s}:  {1:>12.6f}'.format('Rate', m.rate()))
-    print('{0:>25s}:  {1:>12.6f}'.format('Month Growth', m.month_growth()))
-    print('{0:>25s}:  {1:>12.6f}'.format('APY', m.apy()))
-    print('{0:>25s}:  {1:>12.0f}'.format('Payoff Years', m.loan_years()))
-    print('{0:>25s}:  {1:>12.0f}'.format('Payoff Months', m.loan_months()))
-    print('{0:>25s}:  {1:>12.2f}'.format('Amount', m.amount()))
-    print('{0:>25s}:  {1:>12.2f}'.format('Monthly Payment', m.monthly_payment()))
-    print('{0:>25s}:  {1:>12.2f}'.format('Annual Payment', m.annual_payment()))
-    print('{0:>25s}:  {1:>12.2f}'.format('Total Payout', m.total_payout()))
+    print('{0:>25s}:  {1:>12.6f}'.format('Rate', m.rate))
+    print('{0:>25s}:  {1:>12.6f}'.format('Month Growth', m.month_growth))
+    print('{0:>25s}:  {1:>12.6f}'.format('APY', m.apy))
+    print('{0:>25s}:  {1:>12.0f}'.format('Payoff Years', m.loan_years))
+    print('{0:>25s}:  {1:>12.0f}'.format('Payoff Months', m.loan_months))
+    print('{0:>25s}:  {1:>12.2f}'.format('Amount', m.amount))
+    print('{0:>25s}:  {1:>12.2f}'.format('Monthly Payment', m.monthly_payment))
+    print('{0:>25s}:  {1:>12.2f}'.format('Annual Payment', m.annual_payment))
+    print('{0:>25s}:  {1:>12.2f}'.format('Mortgage Payout', m.total_payout))
 
 def main():
     parser = argparse.ArgumentParser(description='Mortgage Amortization Tools')
---
+++ roi.py
@@ -0,0 +1,142 @@
+from mortgage import Mortgage
+
+class RapidPayMortgage(Mortgage):
+  """Mortgage with Rapid pre-payment option
+  supplied prepayment_table needs to reflect prepayment
+  for each month of the mortgage"""
+  def __init__(self, interest, months, 
+                  house_price=None, downpayment=None,
+                  amount=None,
+                  prepayment_table=None
+                 ):
+      Mortgage.__init__(self,interest,months,
+                  house_price=house_price, downpayment=downpayment,
+                  amount=amount
+                 )
+      self.prepayment_table=prepayment_table
+
+
+  def monthly_prepayment(self,month):
+      return self.prepayment_table[month-1]
+
+class ROI:
+    """Mortgage Return on investment calculator. Includes
+    rough calculations of tax returns combined with all 
+    house associated payments and refunds"""
+    def __init__(self,
+                 mortgage,
+                 target_sell_price=None,
+                 appreciation=1,
+                 baseline_return=None,
+                 investments=0,
+                 property_tax=0,
+                 property_insurance=0,
+                 tax_rate=0.4
+                ):
+        self._mortgage=mortgage
+        if target_sell_price is None:
+            self._target_sell_price=self.mortgage.house_price
+        else: self._target_sell_price=target_sell_price
+        self._appreciation=appreciation
+        if baseline_return is None:
+            # at the minimum we want our downpayment back
+            self._baseline_return=self.mortgage.house_price-self.mortgage.amount
+        else:
+            self._baseline_return=baseline_return
+        self._investments=investments
+        self._property_insurance=property_insurance
+        self._property_tax=property_tax
+        self._tax_rate=tax_rate
+
+    @property
+    def mortgage(self):
+        return self._mortgage
+
+    @property
+    def target_sell_price(self):
+        return self._target_sell_price
+
+    @property
+    def appreciation(self):
+        return self._appreciation
+
+    @property
+    def baseline_return(self):
+        return self._baseline_return
+
+    @property
+    def investments(self):
+        return self._investments
+
+    @property
+    def property_tax(self):
+        return self._property_tax
+
+    @property
+    def property_insurance(self):
+        return self._property_insurance
+
+    @property
+    def tax_rate(self):
+        return self._tax_rate
+
+    def monthly_table(self):
+        house_tax_monthly=float(self.mortgage.house_price)*self.property_tax/12
+        insurance_monthly=self.property_insurance/12
+        write_off_gain=0
+        principal_remaining=self.mortgage.amount
+
+        principal_paid=float(0)
+        interest_paid=float(0)
+        insurance_paid=float(0)
+        mortgage_paid=float(0)
+        # house_tax_paid=float(0)
+        target_net_worth=self.baseline_return+self.investments
+
+        year=0
+        month=0
+
+        for index, payment in enumerate(self.mortgage.monthly_payment_schedule()):
+          principal, interest = payment
+          month=(index%12)+1
+          year=(index/12)
+          mortgage_paid+=float(principal)+float(interest)
+          principal_remaining=float(principal_remaining)-float(principal)
+          principal_paid=float(principal_paid)+float(principal)
+          interest_paid=float(interest_paid)+float(interest)
+
+          month_appreciation_delta=month*float(self.mortgage.house_price)*(self.appreciation**(year-1))*(self.appreciation - 1)/12
+
+          monthly_writeoff_gain=(float(interest)+float(house_tax_monthly))*self.tax_rate
+
+          # house_tax_paid+=house_tax_monthly
+          insurance_paid+=insurance_monthly
+          write_off_gain+=monthly_writeoff_gain
+          
+          # net_worth_gain=self.target_sell_price*0.93-principal_remaining-target_net_worth-house_tax_paid-insurance_paid+write_off_gain
+          net_worth_gain=self.target_sell_price*0.93-principal_remaining-target_net_worth
+
+          appreciated_price=float(self.mortgage.house_price)*self.appreciation**(year-1)+month_appreciation_delta
+          # appreciated_net_worth_gain=appreciated_price*0.93-principal_remaining-target_net_worth-house_tax_paid-insurance_paid+write_off_gain
+          appreciated_net_worth_gain=appreciated_price*0.93-principal_remaining-target_net_worth
+          
+          monthly_expence_avg=(net_worth_gain)/(index+1)
+
+          # all_paid_monthly (mortgage+tax+insurance)
+          # principal_remaining
+          # sale_gain (surplus_money-insurance_paid-house_tax_paid)
+          # net_worth_gain (surplus_money)
+          # appreciated_net_worth_gain (surplus_money+potential_price_gain)
+          row=(year, month, 
+                  principal,
+                  interest,
+                  house_tax_monthly,
+                  insurance_monthly,
+                  monthly_writeoff_gain,
+                  net_worth_gain,
+                  appreciated_net_worth_gain,
+               )
+
+          yield row
+          
+
